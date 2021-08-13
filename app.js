ecded391686febc000b6bc7d820d5c2356c7fb13
kurniawans putra <kurniawansputra02@gmail.com>
2021-08-13 19:36:40 +0700

    update

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-const fs = require('fs');
-const { Client } = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal');
 
+require('dotenv').config();
+const fs = require('fs');
+const { Client, MessageMedia } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+const TelegramBot = require('node-telegram-bot-api');
+const token = '-----';// Token Bot TELE
+const bot = new TelegramBot(token, {polling: true});
 
 const SESSION_FILE_PATH = './session.json';
 let sessionCfg;
 if (fs.existsSync(SESSION_FILE_PATH)) {
     sessionCfg = require(SESSION_FILE_PATH);
 }
-var gorupName = "Test"; //nama gorup Crypto ngobrol v2 2021
-var key = "kick"; // pesan perintah 
+var groupName = "Test"; //nama gorup WA
+
+
+const idGroup =   process.env.ID;
+
 const client = new Client({ puppeteer: { headless: true }, session: sessionCfg });
+process.env["NTBA_FIX_350"] = 1;
+
 
 
 client.on('qr', (qr) => {
@@ -24,71 +33,122 @@ client.on('authenticated', (session) => {
     fs.writeFile('./session.json', JSON.stringify(session), function (err) {
         if (err) {
             console.error(err);
+
         }
     });
 });
-
+console.log('Ketik /set di group yang di inging kan, lalu kirim pesan ke bot');
 client.on('ready', () => {
     console.log('Client is ready!');
-    console.log('BOT CHACKET COUNT WA');
-
-    console.log('Bot harus admin grup untuk auto kick');
-
-    console.log('send "'+key+'-jumalh minimal chat" tanpa spasi dan tanda kutip');
-    console.log('Jika ada yang send ke group sesuai format di atas maka bot akan jalan');
 });
 
-client.on('message', async msg => {
-    let chat = await msg.getChat();
-    if (chat.isGroup) {
-        chat.setMessagesAdminsOnly()
-        var m = msg.body.split('-');
-        if (chat.name == gorupName && m[0].toLowerCase() == key.toLowerCase() ) {
-            console.log(`\x1b[42m \x1b[30m *Group Details* \x1b[0m
-Name: ${chat.name}
-Created At: ${chat.createdAt.toString()}
-Created By: ${chat.owner.user}
-Participant count: ${chat.participants.length}
-`);
-            const data = [];
+bot.onText(/\/set/, (msg, match) => {
+    const chatId = msg.chat.id;
+    const resp = match[1]; // the captured "whatever"
+    bot.getChat(chatId).then(data=>{
+        console.log(data.id);
+        fs.unlinkSync('.env')
+        fs.writeFileSync('.env', 'ID='+data.id);
+    })
+    // console.log(bot.getChat(chatId).chat.id)
+    // send back the matched "whatever" to the chat
+    // bot.sendMessage(chatId, resp);
+  });
+  
 
-            var limit = {"limit":100000000000000}
-            console.log("Scraping chat Start")
-            let list = await chat.fetchMessages(limit);
-            console.log("Scraping chat Finish")
-            list.forEach( v=>{
-                if (v.from.search("-") > 0) {
-                    var a = v.from.split("-")
-                    if (data[a[0]] >= m[1]) {
-                        return;
-                    }
-                    data[a[0]] = (data[a[0]] || 0)+1;
-                }else{
-                    var a = v.from.split("@")
-                    if (data[a[0]] >= m[1]) {
-                        return;
-                    }
-                    data[a[0]] = (data[a[0]] || 0)+1;
-                }
-            })
-
-            console.log(data);
-            chat.participants.forEach( async (v,i) => {
-                var a = v.id['user']
-                var c = i++;
-                var c = c+1
-                if (data[a] < m[1] || !data[a]) {
-                    // chat.removeParticipants([v.id.user+"@"+v.id.server]) //Auto kick Hilangkan komen untuk mengaktifkan
-                    console.log(c+" User "+a+" - \x1b[41m\x1b[37m chat "+data[a] + " [Kicked] \x1b[0m | admin :" + v.isAdmin + " | Super Admin :" + v.isSuperAdmin)
-
-                }else{
-                    // msg.reply( a+" - chat "+data[a] + " [Save] ");    // auto replay hikangkan komen utnuk mengaktifkan
-                    console.log(c+" User "+a+" - \x1b[42m\x1b[30m chat "+data[a] + " [Save] \x1b[0m| admin :" + v.isAdmin + " | Super Admin :" + v.isSuperAdmin)
-                }
-
-            })
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    if (msg.photo) {
+        if (!fs.existsSync('photos')){
+            fs.mkdirSync('photos', { recursive: true });
         }
+        
+        const file_id = msg.photo[3].file_id;
+        const caps = msg.caption;
+        bot.getFile(file_id).then(function (resp) {
+                bot.downloadFile(file_id, "photos/").then(function (resps) {
+                resps
+                });
+                async function group(cap = '',file) {
+                    const group =  await client.getChats().then(chats => {
+                        chats.forEach(v => {
+                            if (v.name == groupName) {
+                                // console.log(v.id._serialized)
+                                const media = MessageMedia.fromFilePath('./'+file);
+                                client.sendMessage(v.id._serialized, media,{caption: cap}).then(resp =>{
+                                    resp
+                                })
+                               
+                                    
+                                
+                                
+                            }
+                        });
+                    });
+                    const fileOptions = {
+                        // Explicitly specify the file name.
+                        filename: 'customfilename',
+                        // Explicitly specify the MIME type.
+                        contentType: 'image/jpeg',
+                      };
+                    bot.sendPhoto(idGroup,fs.createReadStream('./'+file),{caption : cap} ,fileOptions).catch((error) => {
+                        console.log(error.code);  // => 'ETELEGRAM'
+                        console.log(error.response.body); // => { ok: false, error_code: 400, description: 'Bad Request: chat not found' }
+                      });
+                }
+                group(caps,resp.file_path);
+                
+        });
     }
+    if (msg.video) {
+        if (!fs.existsSync('videos')){
+            fs.mkdirSync('videos', { recursive: true });
+        }
+        const file_id = msg.video.file_id;
+        const caps = msg.caption;
+        bot.getFile(file_id).then(function (resp) {
+            // console.log(resp);
+                bot.downloadFile(file_id, "videos/").then(function (resps) {
+                });
+                async function group(cap = '',file) {
+                    const group =  await client.getChats().then(chats => {
+                        chats.forEach(v => {
+                            if (v.name == groupName) {
+                               
+                                const media = MessageMedia.fromFilePath('./'+file);
+                                client.sendMessage(v.id._serialized, media,{caption: cap}).then(resp =>{
+                                    // console.log(resp);
+                                    resp
+                                })
+                                
+                            }
+                        });
+                    });
+                }
+                group(caps,resp.file_path);
+        });
+    }
+
+    if (msg.text && !msg.entities) {
+        
+                async function group(file) {
+                    const group =  await client.getChats().then(chats => {
+                        chats.forEach(v => {
+                            if (v.name == groupName) {
+                                var chat = `*Chat from ${msg.from.username}*
+${file}`
+                                client.sendMessage(v.id._serialized, chat,{caption: "cap"}).then(resp =>{
+                                    resp
+                                })
+                                bot.sendMessage(idGroup, file);
+                            }
+                        });
+                    });
+                }
+                group(msg.text);
+    }
+    // console.log(msg);
 });
+
 
 client.initialize();
